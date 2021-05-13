1108eb991ef7c812fc7eeff075023d7786802963
Yurii19 <yurkomits@gmail.com>
2021-05-13 20:26:43 +0300

    feature: added link to git hub repo

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,9 @@ import "./styles.css";
 import CalcUI from "./components/CalcUI";
 export default function App() {
   return (
+    
     <div className="App">
+      <h2><a href="https://github.com/Yurii19/calculator-raw" target='blank'>Git Hub</a></h2>
       <CalcUI />
     </div>
   );
